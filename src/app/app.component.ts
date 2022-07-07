01a29b3ac1d895c6152c63ad27404bf25b9569ac
Andreea <andreiandreea749@gmail.com>
2022-07-07 18:12:26 +0300

    Modify title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Demo';
+  title = 'NewTitle';// de aici modificam titlul aplicatiei
 }
+
